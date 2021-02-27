24bc345b6e805bc433f3328093cb48bd4a42f9a9
Stein Haugan <s.v.h.haugan@astro.uio.no>
2021-02-27 09:46:38 +0100

    Update single_capture

--- single_capture.sh
+++ single_capture.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function idle_seconds() {
+  # Echo - thus return!
+  local float_seconds=`ioreg -c IOHIDSystem | awk '/HIDIdleTime/ {print $NF/1000000000; exit}'`
+  echo ${float_seconds%.*}
+}
+
+cadence=120
+idle_threshold=$((20 * 60))
+
 date=`date "+%Y-%m-%d-%H:%M:%S"`
 file=~/screencapture/"$date".png
 idle_seconds=`idle_seconds`
